e1fb1f9844a4c1417f99b5d621cf0e8cf84b551e
Svasquezs1-EAFIT <sebasvas2022@gmail.com>
2023-05-28 20:03:22 -0500

    Update

--- USER_LOGUEO/index.php
+++ USER_LOGUEO/index.php
@@ -1,3 +1,21 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./Styles.css">
+</head>
+<body>
+    <form method="post">
+    	<h1>¡Suscribete!</h1>
+    	<input type="text" name="name" placeholder="Nombre completo">
+    	<input type="email" name="email" placeholder="Email">
+    	<input type="submit" name="register">
+    </form>
+    <?php 
+        include("registrar.php");
+    ?>
+</body>
+</html>
