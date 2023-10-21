5f448f9fb56a4f9ea7d00070a56732659b2c61ec
Svasquezs1-EAFIT <sebasvas2022@gmail.com>
2023-10-21 17:27:06 -0500

    Update

---
+++ Pagina_Juan/Codigo/Index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La paruma</title>
+    <link rel="stylesheet" href="./Styles.css">
+    <link rel="icon" href="./icono_paruma.png">
+</head>
+<body>
+
+    <header>
+        <div id="logo">
+            <img class="Logo" src="./icono_paruma.png" alt="Logo">
+        </div>
+
+        <nav>
+            <a href="#">Quienes Somos</a>
+            <a href="#">A que nos dedicamos</a>
+            <a href="#footer">contactenos</a>
+        </nav>
+    </header>
+
+    <div>
+        <a href="https://api.whatsapp.com/send?phone=3107106232" target="_blank"><img src="./whatsapp.png" class="whatsapp" alt="icono_whatsapp" ></a>  
+    </div>
+
+    <div class="carrucel">
+        <ul>
+            <li>
+                <img src="./img_1.jpg" alt="">
+            </li>
+            <li>
+                <img src="./img_2.jpg" alt="">
+            </li>
+            <li>
+                <img src="./img_3.jpg" alt="">
+            </li>
+            <li>
+                <img src="./img_4.jpg" alt="">
+            </li>
+        </ul>
+    </div>
+
+    <footer class="footer" id="footer">
+        <div class="footer-content container">
+
+            <div class="link">
+                <h3>Contactanos</h3>
+                <ul>
+                    <li><a href="#">Telefono:</a></li>
+                    <li><a href="#">Celular: 3107106232</a></li>
+                    <li><a href="#">Correo: informacionlaparuma@gmail.com</a></li>
+                    <li><a href="#">Whatsapp: 3107106232</a></li>
+                </ul>
+            </div>
+
+            <div class="link">
+                <h3>Pregunta Por</h3>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                </ul>
+            </div>
+
+            <div class="icono">
+                <img src="./icono_paruma.png" alt="">
+            </div>
+        </div>
+    </footer>
+    
+</body>
+</html>
+
